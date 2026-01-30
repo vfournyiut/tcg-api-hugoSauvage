5f28b1d424bc8ac4395d098afd2df7821b56ebe4
hugoSauvage <126684588+hugoSauvage@users.noreply.github.com>
2026-01-30 09:36:29 +0100

    authentificationEtSécurisation
    POST /api/auth/sign-up :
    Inscription réussie avec données valides (email, username, password) → 201
    
    Retourne un token JWT valide
    
    Retourne les infos utilisateur (sans le mot de passe)
    
    Le mot de passe est hashé en base
    
    Erreur si email déjà utilisé → 409
    
    Erreur si données manquantes/invalides → 400
    
    Erreur serveur → 500
    
    POST /api/auth/sign-in
    Connexion réussie avec identifiants corrects → 200
    
    Retourne un token JWT valide
    
    Retourne les infos utilisateur (sans le mot de passe)
    
    Erreur si email inexistant → 401
    
    Erreur si mot de passe incorrect → 401
    
    Erreur si données manquantes → 400
    
    Erreur serveur → 500

---
+++ prisma/migrations/20260130080739_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `count` on the `DeckCard` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "DeckCard" DROP COLUMN "count";
